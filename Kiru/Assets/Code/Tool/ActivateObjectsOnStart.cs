d0d8791f4ad33e02db100ac654cb847c4efcbef7
Andreas Edmeier <assertores@web.de>
2020-07-11 23:50:58 +0200

    added activate tool

---
+++ Kiru/Assets/Code/Tool/ActivateObjectsOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsserTOOLres {
+	public class ActivateObjectsOnStart : MonoBehaviour {
+		[SerializeField] GameObject[] activeObjects;
+		[SerializeField] GameObject[] disabledObjects;
+		void Start() {
+			foreach(var it in activeObjects) {
+				it.SetActive(true);
+			}
+			foreach(var it in disabledObjects) {
+				it.SetActive(false);
+			}
+
+			Destroy(this);
+		}
+	}
+}
