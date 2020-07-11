55ea67f37a9f700b47db29a3a0f130c16c57412b
Andreas Edmeier <assertores@web.de>
2020-07-11 21:17:45 +0200

    added quit

---
+++ Kiru/Assets/Code/Controler/QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kiru {
+	public class QuitGame : ICommand {
+		public override void Execute() {
+			Application.Quit();
+#if UNITY_EDITOR
+			Debug.Break();
+#endif
+		}
+	}
+}
---
+++ Kiru/Assets/Code/Controler/ReactToKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kiru {
+	public class ReactToKey : MonoBehaviour {
+		
+		[SerializeField] ICommand _command = null;
+		[SerializeField] KeyCode _key = KeyCode.Escape;
+
+		private void Update() {
+			if(Input.GetKeyUp(_key)) {
+				_command.Execute();
+			}
+		}
+	}
+}
