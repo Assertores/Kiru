052ce0f7da8c578db00a4ed0ec06496c05200325
Andreas Edmeier <assertores@web.de>
2020-07-11 17:23:43 +0200

    added lower limit to growth

---
+++ Kiru/Assets/Code/Model/DontGrowUnterPlain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kiru {
+	public class DontGrowUnterPlain : IGrothValidate {
+		public override bool Validate(IBranch element, Transform slot) {
+			return slot.position.y > transform.position.y;
+		}
+	}
+}
