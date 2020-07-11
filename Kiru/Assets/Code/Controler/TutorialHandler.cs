a24cd65ef4952ec054f9b62c7822f66f6cd45081
Andreas Edmeier <assertores@web.de>
2020-07-11 21:17:55 +0200

    added tutorial capability

---
+++ Kiru/Assets/Code/Controler/TutorialHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kiru {
+	public class TutorialHandler : ICommand {
+		static bool _hasSeenTutorial = false;
+
+		[SerializeField] GameObject _tutorialHolder = null;
+
+		public override void Execute() {
+			_hasSeenTutorial = true;
+			_tutorialHolder.SetActive(false);
+		}
+
+		private void Start() {
+			_tutorialHolder.SetActive(!_hasSeenTutorial);
+		}
+	}
+}
