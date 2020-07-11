a34c19a8fe20ccf270f8488839bf5ad6a647d430
Andreas Edmeier <assertores@web.de>
2020-07-11 13:50:01 +0200

    added cut animation and sound

---
+++ Kiru/Assets/Code/View/PlayRandomAudioClip.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kiru {
+	public class PlayRandomAudioClip : MonoBehaviour {
+
+		[SerializeField] AudioSource _target;
+		[SerializeField] AudioClip[] _clips;
+
+		public void Execute() {
+			_target.PlayOneShot(_clips[Random.Range(0, _clips.Length)]);
+		}
+	}
+}
