3208947cf923c4e4edc5f7e11e3010d6c337f258
Andreas Edmeier <assertores@web.de>
2020-07-12 14:31:14 +0200

    tutorial dynamic

---
+++ Kiru/Assets/Code/View/SyncActivation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kiru {
+	public class SyncActivation : MonoBehaviour {
+		[SerializeField] GameObject _target;
+
+		private void Awake() {
+			_target.SetActive(gameObject.activeSelf);
+		}
+
+		private void OnEnable() {
+			_target.SetActive(true);
+		}
+		private void OnDisable() {
+			_target.SetActive(false);
+		}
+	}
+}
